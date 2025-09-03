df11c71363fa4442463d4772f12bff8bf3c3043f
fredy estiven diaz pinilla <fredyestivendiazpinilla3@gmail.com>
2025-09-02 20:17:38 -0500

    repasojs

---
+++ 01-HolaMundo/holamundo.js
@@ -0,0 +1,31 @@
+console.log('Hola mundo');
+
+let cajaDeAmdy = 'Woody';
+
+console.log(cajaDeAmdy);
+
+let edad = 62;
+let nombre = 'Estiven Diaz';
+
+
+console.log('Mi nombre es '+nombre+" y tengo "+edad+" años.");
+
+//array
+
+let frutas = ['manzana', 'banano', 'mango'];
+
+console.log (frutas);
+
+console.log(frutas[1]);
+
+let frase = "JavaScript es extremadamente genial";
+
+console.log(frase);
+console.log(frase.length);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+//operaciones matematicas 
+const suma = +12;
+
+console.log(suma);
